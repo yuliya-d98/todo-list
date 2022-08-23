d4efa16e4077fd1b537ee513d5f9c58167d212a6
Yuliya Dubovik <feelgloom@gmail.com>
2022-08-23 15:16:38 +0300

    error page

--- src/pages/errorPage.tsx
+++ src/pages/errorPage.tsx
@@ -1,7 +1,19 @@
-import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 
 const ErrorPage = () => {
-  return <Box>Error Page</Box>;
+  return (
+    <Typography
+      variant="h6"
+      sx={{
+        mt: '20px',
+        mb: '10px',
+        maxWidth: { xs: '250px', custom450: '400px', md: '800px' },
+        mx: 'auto',
+      }}
+    >
+      Oops! Some error occured.
+    </Typography>
+  );
 };
 
 export default ErrorPage;
